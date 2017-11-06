c92edb2e0d4b5f9997d7bab5861610f887ef5573
peter soleeh <petersoleeh@gmail.com>
2017-11-06 05:54:30 +0300

    define the different routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,8 @@
 from flask import render_template
 from . import main
+from .. import db
+from ..models import Article, Comment, User, Writer
+
 
 
 @main.route('/')
@@ -7,6 +10,20 @@ def index():
     '''
     landing page
     '''
+    # get the articles/blogs title and date posted
+
+    article = Article.get_articles()
 
     title = 'Prr-Blog'
-    return render_template('index.html',title = title)
+
+    return render_template('index.html',title = title,article=article)
+
+
+@main.route('/article/<int:id>')
+def article(id):
+
+    # article = Article.query.get(id)
+    # comments = Comment.get_comments(article.id)
+    # title = f' {article.title}'
+
+    return render_template('article-detail.html',id=id)
