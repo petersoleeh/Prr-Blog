626a5a5488ccb4c373ff45fecefde20f1e113dd9
peter soleeh <petersoleeh@gmail.com>
2017-11-07 05:04:02 +0300

    add update class user

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from . import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 
 class Article(db.Model):
@@ -24,13 +27,28 @@ class Article(db.Model):
 
 
 
-class User(db.Model):
+class User(db.Model,UserMixin):
 
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
     comments = db.relationship('Comment',backref = 'user', lazy = "dynamic")
+    pass_secure = db.Column(db.String(255))
+    email = db.Column(db.String,unique=True,index=True)
+
+    @property
+    def password(self):
+        raise AttributeError('you cannot read the password Attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure,password)
+
+
 
 
     def __repr__(self):
